37852346ffbdda94380238daa9ea4fe049d11be2
eransa <eransafadel@gmail.com>
2022-05-29 14:07:04 +0300

    current Guess

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,16 +1,50 @@
-import React from 'react';
-import styles from './Row.module.css';
+import React from "react";
+import styles from "./Row.module.css";
 
-const Row =()=> {
+const Row = ({ guess,currentGuess }) => {
+  if (guess) {
+    return (
+      <div className={styles["row"]}>
+        {guess.map((l, i) => {
+          return (
+            <div key={i} className={styles[l.color]}>
+              {l.key}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  if(currentGuess){
+      let letters = currentGuess.split('');
+      return(
+          <div className={styles["row"]}>
+              {letters.map((letter,i)=>{
+                  return <div key={i} className={styles["filled"]}>{letter} </div> 
+              })}
+              {[...Array(5-letters.length)].map((_,i)=>{
+                  return <div key={i}/>
+                  })}
+
+          </div>
+      )
+
+  }
+
+
+
+
+
   return (
-    <div className={styles['row']}>
-        <div> </div>
-        <div> </div>
-        <div> </div>
-        <div> </div>
-        <div> </div>
+    <div className={styles["row"]}>
+      <div> </div>
+      <div> </div>
+      <div> </div>
+      <div> </div>
+      <div> </div>
     </div>
-  )
-}
+  );
+};
 
 export default Row;
